3b0ea593263cd9d9755c099d30879c107db0f9ea
Stad07 <stad-777@yandex.ru>
2022-11-19 14:32:10 +0300

    add react-notes project

---
+++ src/pages/Home.jsx
@@ -0,0 +1,31 @@
+import React, { useContext, useEffect } from 'react'
+import { Form } from '../components/Form'
+import { Notes } from '../components/Notes'
+import { Loader } from '../components/Loader'
+import { FirebaseContext } from '../context/firebase/firebaseContext'
+
+export const Home = () => {
+  const {loading, notes, fetchNotes, removeNote} = useContext(FirebaseContext)
+
+  useEffect(() => {
+    fetchNotes()
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  return (
+    <>
+      <Form />
+
+      <hr/>
+
+      { loading ? (
+          <Loader />
+        ) : (
+          <Notes notes={notes} onRemove={removeNote}/>
+        )  
+      }
+
+      
+    </>
+  )
+}
